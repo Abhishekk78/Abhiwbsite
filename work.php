d7646936c25211639dd66c005697c754cb4f55e9
Abhishekk78 <147427041+Abhishekk78@users.noreply.github.com>
2023-10-09 22:10:19 +0530

    added work.php

---
+++ work.php
@@ -0,0 +1,288 @@
+<center>
+  <section class="my-5">
+
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="cols-lg-6 col-md-6 col-12">
+      <img src="images/12.jpg" class="image-fluid Aboutimg">
+
+    </div>
+    <div class="cols-lg-6 col-md-6 col-12"><br>
+    <br>
+      <h2 class="display-4 " style="color:dodgerblue;" >Web Development
+      <br> HI I AM ABHISHK PANDYA . <br> I AM WEBDEVELOPER AND PROBLEM SOLVER</h2>
+      <p class="py-3"> </p>
+
+    </div>
+
+  </div>
+</div>
+</section>
+</center>
+ 
+
+  <section class="my-5">
+
+    <div class="text-center">
+      <div class="py-5">
+        <h2 style="color:dodgerblue;">About</h2>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="cols-lg-6 col-md-6 col-12">
+          <img src="images/7.jpg" class="image-fluid Aboutimg">
+
+        </div>
+        <div class="cols-lg-6 col-md-6 col-12">
+          <h2 class="display-4">Web Development</h2>
+          <p class="py-3"> HI i am abhishek pandya. i am a Webdeveloper to create Beautiful websites.Using Front-end Html,Css,javascript,Vuejs,Angularjs,Bootstrap.
+            Using Back-end Php Language. </p>
+          <a href="About.php" class="btn btn-success">Read more</a>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section class="my-5">
+
+    <div class="text-center">
+      <div class="py-5">
+        <h2 style="color:dodgerblue;">Good Skills Make Good Career</h2>
+      </div>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-3 col-md-3 col-9">
+
+            <div class="card">
+              <img class="card-img-top" src="images/13.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Git And Github </h4>
+                <p class="card-text">I am good use of git and github</p>
+                <a href="#" class="btn btn-primary">See Profile</a>
+              </div>
+            </div>
+
+
+          </div>
+
+
+          <div class="col-lg-3 col-md-3 col-9">
+
+            <div class="card">
+              <img class="card-img-top" src="images/24.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Vuejs</h4>
+                <p class="card-text">I am good use of Vuejs to develop front-end design</p>
+                <a href="#" class="btn btn-primary">See Profile</a>
+              </div>
+            </div>
+
+
+          </div>
+
+
+          <div class="col-lg-3 col-md-3 col-9">
+
+            <div class="card">
+              <img class="card-img-top" src="images/29.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Angularjs </h4>
+                <p class="card-text">I am good use of Angularjs to develop front-end design</p>
+                <a href="#" class="btn btn-primary">See Profile</a>
+              </div>
+            </div>
+
+
+          </div>
+
+          <div class="col-lg-3 col-md-3 col-9">
+
+            <div class="card">
+              <img class="card-img-top" src="images/14.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Firebase </h4>
+                <p class="card-text">I am good use of Firebase to create database and written query</p>
+                <a href="#" class="btn btn-primary">See Profile</a>
+              </div>
+            </div>
+
+
+          </div>
+
+
+        </div>
+      </div>
+
+  </section>
+
+
+  <section class="my-5">
+
+    <div class="text-center">
+      <div class="py-5">
+        <h2 style="color:dodgerblue;">MY WORK SECTION</h2>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+
+        <div class="col-lg-3 col-md-3 col-9">
+
+          <img src="images/15.png" class="img-fluid pb-4s">
+          <h3>Gujarat tourisam using php</h3>
+          <p>Gujarat is a state along the western coast of India. Its coastline of about 1,600 km is the longest in the country,
+            most of which lies on the Kathiawar peninsula. Gujarat is the fifth-largest Indian state by area, covering some 196,024 km²;
+            and theninth-most populous state, with a population of 60.4 million</p>
+
+
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-9">
+
+          <img src="images/16.png" class="img-fluid pb-4s">
+          <h3>Currency converte using Angularjs</h3>
+          <p>AngularJS currency filter is used to convert a number into a currency format.
+            If no currency format is specified currency filter uses the local currency format.
+            Parameters: It contains 2 parameters as mentioned above and described below: symbol:
+            It is an optional parameter.</p>
+
+
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-9">
+
+          <img src="images/17.png" class="img-fluid pb-4s">
+          <h3>Login page using vuejs</h3>
+          <p>VueJS is the first choice in my opinion if you want to construct a single-page application. Because there are so many challenges in the development area that cannot be handled by utilizing a single library, VueJS is interoperable with different libraries, so you may easily use it. VueJS is supported by all major browsers, including Chrome, Firefox, Internet Explorer, Safari, and others. This library may easily be compared to your favorite libraries.</p>
+
+
+        </div>
+
+
+
+      </div>
+    </div>
+  </section>
+
+  <section class="my-5">
+
+    <div class="text-center">
+      <div class="py-5">
+        <h2 style="color:dodgerblue;">MY SKILLS AND EXPERIENCES</h2>
+      </div>
+<br>
+<div class="container">
+  
+  <div class="btn-group">
+    <button type="button" class="btn btn-primary"><a href="work.php"></a>All</button> 
+    <button type="button" class="btn btn-primary"><a href="work.php"></a> Webdevelopment</button>
+    <button type="button" class="btn btn-primary"><a href="work.php"></a>  GujartPortal</button>
+    <button type="button" class="btn btn-primary"><a href="work.php"></a>  Login page</button>
+    <button type="button" class="btn btn-primary"><a href="work.php"></a>  CurrencyConverter</button>
+  </div>
+ 
+
+
+
+
+
+<br><br><br>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-2 col-md-2 col-8">
+
+            <div class="card">
+              <img class="card-img-top" src="images/31.jpg" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Html5 </h4>
+                <p class="card-text"></p>
+
+              </div>
+            </div>
+
+
+          </div>
+
+          <div class="col-lg-2 col-md-2 col-8">
+
+            <div class="card">
+              <img class="card-img-top" src="images/38.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Css3 </h4>
+                <p class="card-text"></p>
+
+              </div>
+            </div>
+
+
+          </div>
+
+
+          <div class="col-lg-2 col-md-2 col-8">
+
+            <div class="card">
+              <img class="card-img-top" src="images/40.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">javascript </h4>
+                <p class="card-text"></p>
+
+              </div>
+            </div>
+
+
+          </div>
+
+          <div class="col-lg-2 col-md-2 col-8">
+
+            <div class="card">
+              <img class="card-img-top" src="images/35.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Angular </h4>
+                <p class="card-text"></p>
+
+              </div>
+            </div>
+
+
+          </div>
+
+          <div class="col-lg-2 col-md-2 col-8">
+
+            <div class="card">
+              <img class="card-img-top" src="images/34.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Vuejs </h4>
+                <p class="card-text"></p>
+
+              </div>
+            </div>
+
+
+          </div>
+
+          <div class="col-lg-2 col-md-2 col-8">
+
+            <div class="card">
+              <img class="card-img-top" src="images/41.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Bootstrap </h4>
+                <p class="card-text"></p>
+
+              </div>
+            </div>
+
+
+          </div>
+
+
+  </section>
+  <script>
+  alert('WORK PAGE');
+</script>
