a7cf50e7822f61ad2eae8d3424f7ef7768e74b28
Abhishekk78 <147427041+Abhishekk78@users.noreply.github.com>
2023-10-09 21:56:27 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,503 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/CSS?family=Josefin+Sans&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384- 
+    lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    
+  <title>Abhishek Website </title>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">AbhiTechnical</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="About.php">ABOUT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="a8.php">SKILLS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="work.php">WORK</a>
+          </li>
+          
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+
+          <button class="btn btn-outline-success" type="submit"><a href="https://www.google.com"></a>search</button>
+
+        </form>
+        <form class="d-flex">
+
+
+          <button class="btn btn-outline-success" type="register"><a href="Registration.php" style="color:seagreen; text-decoration:none">Registration</a></button>
+
+        </form>
+        <form class="d-flex">
+
+
+          <button class="btn btn-outline-success" type="login"><a href="login.php" style="color:seagreen; text-decoration:none;">Login</a></button>
+
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <div id="demo" class="carousel slide" data-ride="carousel">
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+      <li data-target="#demo" data-slide-to="2"></li>
+    </ul>
+    <div class="carousel-inner">
+
+
+
+
+      <div class="carousel-item active">
+        <img src="images/6.jpg" alt="image2" width="1600" height="670">
+        <div class="carousel-caption">
+
+          <h3 style="color: red;">Computer Engineer </h3>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="images/5.jpg" alt="image3" width="1600" height="670">
+        <div class="carousel-caption">
+
+          <h3 style="color: red;">Webdevlopment</h3>
+
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="images/4-removebg-preview.jpg" alt="image4" width="1600" height="670">
+        <div class="carousel-caption">
+
+          <h3 style="color: red;">About Some Language</h3>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+  </div>
+
+  <center>
+    <section class="my-5">
+
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="cols-lg-6 col-md-6 col-12">
+            <img src="images/12.jpg" class="image-fluid Aboutimg">
+
+          </div>
+          <div class="cols-lg-6 col-md-6 col-12"><br>
+            <br>
+            <h2 class="display-4 " style="color:dodgerblue;">Web Development
+              <br> HI I AM ABHISHK PANDYA . <br> I AM WEBDEVELOPER AND PROBLEM SOLVER
+            </h2>
+            <p class="py-3"> </p>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+  </center>
+
+
+  <section class="my-5">
+
+    <div class="text-center">
+      <div class="py-5">
+        <h2 style="color:dodgerblue;">About</h2>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="cols-lg-6 col-md-6 col-12">
+          <img src="images/7.jpg" class="image-fluid Aboutimg">
+
+        </div>
+        <div class="cols-lg-6 col-md-6 col-12">
+          <h2 class="display-4">Web Development</h2>
+          <p class="py-3"> HI i am abhishek pandya. i am a Webdeveloper to create Beautiful websites.Using Front-end Html,Css,javascript,Vuejs,Angularjs,Bootstrap.
+            Using Back-end Php Language. </p>
+          <a href="8.php" class="btn btn-success">Read more</a>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section class="my-5">
+
+    <div class="text-center">
+      <div class="py-5">
+        <h2 style="color:dodgerblue;">Good Skills Make Good Career</h2>
+      </div>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-3 col-md-3 col-9">
+
+            <div class="card">
+              <img class="card-img-top" src="images/13.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Git And Github </h4>
+                <p class="card-text">I am good use of git and github</p>
+                <a href="a7.php" class="btn btn-primary">See Profile</a>
+              </div>
+            </div>
+
+
+          </div>
+
+
+          <div class="col-lg-3 col-md-3 col-9">
+
+            <div class="card">
+              <img class="card-img-top" src="images/24.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Vuejs</h4>
+                <p class="card-text">I am good use of Vuejs to develop front-end design</p>
+                <a href="a4.php" class="btn btn-primary">See Profile</a>
+              </div>
+            </div>
+
+
+          </div>
+
+
+          <div class="col-lg-3 col-md-3 col-9">
+
+            <div class="card-fluid">
+              <img class="card-img-top" src="images/29.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Angularjs </h4>
+                <p class="card-text">I am good use of Angularjs to develop front-end design</p>
+                <a href="a5.php" class="btn btn-primary">See Profile</a>
+              </div>
+            </div>
+
+
+          </div>
+
+          <div class="col-lg-3 col-md-3 col-9">
+
+            <div class="card">
+              <img class="card-img-top" src="images/14.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">Firebase </h4>
+                <p class="card-text">I am good use of Firebase to create database and written query</p>
+                <a href="a6.php" class="btn btn-primary">See Profile</a>
+              </div>
+            </div>
+
+
+          </div>
+
+
+        </div>
+      </div>
+
+  </section>
+
+  <center>
+    <section class="my-5">
+
+      <div class="text-center">
+        <div class="py-5">
+          <h2 style="color:dodgerblue;">MY WORK SECTION</h2>
+        </div>
+      </div>
+
+      <div class="container-fluid">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-3 col-9">
+
+            <img src="images/15.png" class="img-fluid pb-4s">
+            <h3>Gujarat tourisam using php</h3>
+            <p>Gujarat is a state along the western coast of India. Its coastline of about 1,600 km is the longest in the country,
+              most of which lies on the Kathiawar peninsula. Gujarat is the fifth-largest Indian state by area, covering some 196,024 km²;
+              and theninth-most populous state, with a population of 60.4 million</p>
+
+
+          </div>
+
+          <div class="col-lg-3 col-md-3 col-9">
+
+            <img src="images/16.png" class="img-fluid pb-4s">
+            <h3>Currency converte using Angularjs</h3>
+            <p>AngularJS currency filter is used to convert a number into a currency format.
+              If no currency format is specified currency filter uses the local currency format.
+              Parameters: It contains 2 parameters as mentioned above and described below: symbol:
+              It is an optional parameter.</p>
+
+
+          </div>
+
+          <div class="col-lg-3 col-md-3 col-9">
+
+            <img src="images/17.png" class="img-fluid pb-4s">
+            <h3>Login page using vuejs</h3>
+            <p>VueJS is the first choice in my opinion if you want to construct a single-page application. Because there are so many challenges in the development area that cannot be handled by utilizing a single library, VueJS is interoperable with different libraries, so you may easily use it. VueJS is supported by all major browsers, including Chrome, Firefox, Internet Explorer, Safari, and others. This library may easily be compared to your favorite libraries.</p>
+
+
+          </div>
+
+
+
+        </div>
+      </div>
+    </section>
+  </center>
+  <section class="my-5">
+
+    <div class="text-center">
+      <div class="py-5">
+        <h2 style="color:dodgerblue;">MY SKILLS AND EXPERIENCES</h2>
+      </div>
+      <br>
+      <div class="container">
+
+        <div class="btn-group">
+          <button type="button" class="btn btn-primary"><a href="a1.php" style="color:white; text-decoration:none;">All</a></button>
+          <button type="button" class="btn btn-primary"><a href="a2.php" style="color:white; text-decoration:none;">Webdevelopment</a></button>
+          <button type="button" class="btn btn-primary"><a href="a3.php" style="color:white; text-decoration:none;">GujartPortal</a></button>
+          <button type="button" class="btn btn-primary"><a href="a4.php" style="color:white; text-decoration:none;">Login page</a></button>
+          <button type="button" class="btn btn-primary"><a href="a5.php" style="color:white; text-decoration:none;">CurrencyConverter</a></button>
+        </div>
+
+        <br><br><br>
+
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-lg-2 col-md-2 col-8">
+
+              <div class="card">
+                <img class="card-img-top" src="images/31.jpg" alt="Card image">
+                <div class="card-body">
+                  <h4 class="card-title">Html5 </h4>
+                  <p class="card-text"></p>
+
+                </div>
+              </div>
+
+
+            </div>
+
+            <div class="col-lg-2 col-md-2 col-8">
+
+              <div class="card">
+                <img class="card-img-top" src="images/38.png" alt="Card image">
+                <div class="card-body">
+                  <h4 class="card-title">Css3 </h4>
+                  <p class="card-text"></p>
+
+                </div>
+              </div>
+
+
+            </div>
+
+
+            <div class="col-lg-2 col-md-2 col-8">
+
+              <div class="card">
+                <img class="card-img-top" src="images/40.png" alt="Card image">
+                <div class="card-body">
+                  <h4 class="card-title">javascript </h4>
+                  <p class="card-text"></p>
+
+                </div>
+              </div>
+
+
+            </div>
+
+            <div class="col-lg-2 col-md-2 col-8">
+
+              <div class="card">
+                <img class="card-img-top" src="images/35.png" alt="Card image">
+                <div class="card-body">
+                  <h4 class="card-title">Angular </h4>
+                  <p class="card-text"></p>
+
+                </div>
+              </div>
+
+
+            </div>
+
+            <div class="col-lg-2 col-md-2 col-8">
+
+              <div class="card">
+                <img class="card-img-top" src="images/34.png" alt="Card image">
+                <div class="card-body">
+                  <h4 class="card-title">Vuejs </h4>
+                  <p class="card-text"></p>
+
+                </div>
+              </div>
+
+
+            </div>
+
+            <div class="col-lg-2 col-md-2 col-8">
+
+              <div class="card">
+                <img class="card-img-top" src="images/41.png" alt="Card image">
+                <div class="card-body">
+                  <h4 class="card-title">Bootstrap </h4>
+                  <p class="card-text"></p>
+
+                </div>
+              </div>
+
+
+            </div>
+
+
+  </section>
+
+
+  <section class="my-5">
+
+    <div class="text-center">
+      <div class="py-5">
+        <h2 style="color:dodgerblue;">Contact Us</h2>
+      </div>
+
+      <div class="w-50  m-auto">
+        <form action="Userinfo.php" method="post">
+          <div class="form-group">
+            <label>Username:</label>
+            <input type="text" name="User" autocomplete="off" class="form-control" placeholder="Enter Your Name" required>
+          </div>
+
+          <div class="form-group">
+            <label>Email Id:</label>
+            <input type="text" name="Email" autocomplete="off" class="form-control" placeholder="Enter Your Email Id" required>
+          </div>
+
+
+
+          <div class="form-group">
+            <label>Mobile No:</label>
+            <input type="text" name="Mobile" autocomplete="off" class="form-control" placeholder="Enter Your Mobile No" required>
+          </div>
+
+          <div class="form-group">
+            <label>Comment:</label>
+            <textarea class="form-control" name="Comment" placeholder="Comment" required></textarea>
+          </div>
+          <button type="submit" class="btn btn-success">Submit</button>
+
+        </form>
+      </div>
+
+  </section>
+
+  <footer>
+    <p class="p-3 bg-dark text-white text-center">@AbhiTechnicalProduction
+    <section class="my-5">
+
+      <center>
+
+        <button type="submit" name="learn" value="myimage">
+          <a href="https://www.whatsapp.com/"><img src="images/whatsapp.png " height="50px" width="50px" style="border-radius: 10px;"></a>
+
+        </button>
+
+        <button type="submit" name="learn" value="myimage">
+          <a href="https://www.instagram.com/"><img src="images/instagram.png " height="50px" width="50px"></a>
+
+        </button>
+
+        <button type="submit" name="learn" value="myimage">
+          <a href="https://twitter.com/"><img src="images/twitter.png " height="50px" width="50px"></a>
+
+        </button>
+
+        <button type="submit" name="learn" value="myimage">
+          <a href="https://www.facebook.com/"><img src="images/facebook.png " height="50px" width="50px"></a>
+
+        </button>
+
+        <button type="submit" name="learn" value="myimage">
+          <a href="https://sg.linkedin.com/"><img src="images/linkedin.png " height="50px" width="50px"></a>
+
+        </button>
+
+        <button type="submit" name="learn" value="myimage">
+          <a href="https://www.google.com.sg/"><img src="images/google.png " height="50px" width="50px"></a>
+
+        </button>
+        <button type="submit" name="learn" value="myimage">
+          <a href="https://www.youtube.com/"><img src="images/youtube.png" height="50px" width="50px"></a>
+
+        </button>
+        </p>
+      </center>
+    </section>
+  </footer>
+
+  <script>
+    alert('WELCOME TO ABHITECH WEBSITE');
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+  <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBI2c5h11NuCZR6wmyNDdpYUbuMUAXrIYY",
+    authDomain: "abhishek-website-f609b.firebaseapp.com",
+    projectId: "abhishek-website-f609b",
+    storageBucket: "abhishek-website-f609b.appspot.com",
+    messagingSenderId: "996023328228",
+    appId: "1:996023328228:web:03dbe339eda28cf3797bf1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
+
+</body>
+
+</html>
+
+<script>
+  alert('HOME PAGE');
+</script>
+
